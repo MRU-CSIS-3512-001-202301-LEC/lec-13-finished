ccff4db463f5804f9969167dce601f5ea058719c
Jordan Pratt <jpratt@mtroyal.ca>
2023-02-27 18:35:41 +0000

    Ready for use.

--- 4-json/js/index.js
+++
@@ -1,6 +0,0 @@
-// careful with the input path
-
-// play with this in the browser's console (right-click > Store object as global variable)
-// Access arrivals. What type is it?
-// Try to access total-count. What's going on here?
-// Find the types of a few things.
